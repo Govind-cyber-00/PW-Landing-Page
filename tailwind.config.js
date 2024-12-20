dbed723c2aaf2b07f8cf00a400d2754529468d11
Govind <govindyadav46324@gmail.com>
2024-12-20 12:32:58 +0530

    upload all files of application

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"], // Adjust based on your project structure
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#081b29', // Define a custom color name
+      },
+    },
+  },
+  plugins: [],
+};
